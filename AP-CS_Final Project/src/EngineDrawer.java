cac41dcb34ac34fed524a95443f559d6f760551f
shaostassen <75188619+shaostassen@users.noreply.github.com>
2021-05-10 11:20:56 -0400

    Add files via upload

---
+++ AP-CS_Final Project/src/EngineDrawer.java
@@ -0,0 +1,16 @@
+
+public class EngineDrawer {
+	//declare all the variables
+    private Maze maze;
+    
+    //constructor
+    public EngineDrawer(Maze maze) {
+		super();
+		this.maze = maze;
+	}
+
+	//use raycasting to draw a 2.5D maze
+    public static boolean drawMaze(Maze maze) {
+    	
+    }
+}
