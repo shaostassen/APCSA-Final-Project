d977f5a3aa381b6a241243026ea203635de71c74
ramananj34 <77083795+ramananj34@users.noreply.github.com>
2021-05-10 11:02:30 -0400

    Add files via upload

---
+++ AP-CS_Final Project/src/Direction.java
@@ -0,0 +1,8 @@
+//An enum for the direction to move in for code clarity. Containds left, right, up, and down
+
+public enum Direction //Direction Enum
+{ //Direction Start
+	
+	UP, DOWN, LEFT, RIGHT //Creates the static options for the enum
+	
+} //Direction End
