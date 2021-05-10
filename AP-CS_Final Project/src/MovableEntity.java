cad49060c017a2cac91353b133398635c897ffe6
ramananj34 <77083795+ramananj34@users.noreply.github.com>
2021-05-10 11:28:07 -0400

    Add files via upload

---
+++ AP-CS_Final Project/src/MovableEntity.java
@@ -0,0 +1,59 @@
+import java.awt.Point; //Imports the Point class which corresponds to the X and Y points on the Maze
+
+public abstract class MovableEntity //This is a template abstract class that can not be initialized
+	implements Entity //Must include the methods in the Entity Interface
+{ //Movable entity start
+
+	protected Point location; //Contains the location of where it is on the graph with public instances X and Y
+	
+	public boolean move(Direction dir, int distance) //Implemented method to move the location
+	{
+		
+		switch (dir) { //Switch the direction for where they choose to go
+		
+		case UP: // If the user wants to go up
+			
+			if (this.location.y == 0) { return false; } //If they are at the top of the map returns false and doesnt move to avoid an ArrayIndexOutOfBoundsException
+			
+			this.location.y = this.location.y-1; // Moves the place up
+			break; //Breaks so it doesnt run the other options
+			
+		case DOWN: //If the user wants to move down
+			
+			if (this.location.y == Maze.getHeight()-1) { return false; } //If they are at the bottom of the map return false and doesnt move to avoid an ArrayIndexOutOfBoundsException
+			
+			this.location.y = this.location.y+1; //Moves the location down
+			break; //Breaks so it does not run the other case scenarios
+			
+		case LEFT: //If they user wants to move to the left
+			
+			if (this.location.x == 0) { return false; } //If they are at the left most position, return false to avoid an ArrayIndexOutOfBounds Exception
+			
+			this.location.x = this.location.x-1; //Moves left
+			break; //Breaks to not run the other scenarios
+			 
+		case RIGHT: //If the user wants to move right
+			
+			if (this.location.x == Maze.getLength()-1) { return false; } //If they are in the right most position, then it returns false to avoid going out of bounds
+			
+			this.location.x = this.location.x+1; //Moves right
+			break; // Breaks to not run other case senarios
+		
+		} //End of switch
+		
+		return true; //If we get to this point, we moved sucsesfully, so we return true
+		
+	} //End of move method
+	
+	public Point getLocation()
+	{
+		return location;
+	}
+	
+	public MovableEntity setLocation(Point newPoint)
+	{
+		location = newPoint;
+		return this;
+	}
+	
+} //Movable entity end
