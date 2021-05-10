e93fb3199af81ec0876520e05cfc4e31432766d7
ramananj34 <77083795+ramananj34@users.noreply.github.com>
2021-05-10 10:56:08 -0400

    Add files via upload

---
+++ AP-CS_Final Project/src/Entity.java
@@ -0,0 +1,11 @@
+import java.awt.Point; //Imports the point class which contains a X and Y locations on the Maze
+import java.util.ArrayList; //Imports a dynamic list object that will store 
+
+public interface Entity //Entity Interface which will be implemented by Abstract class and Treasure
+{ //Entity Start
+
+	public ArrayList<Object> getGraphicsInfo(); //Gets the resources for how to draw the object. I made it an object array because it may contain many different data types
+	
+	public Point getPosition(); //Gives the location on the board in X and Y format
+	
+} //Entity end
