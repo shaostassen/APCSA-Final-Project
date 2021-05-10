da3f75165ae0db48429a47995779f44dab73f152
shaostassen <75188619+shaostassen@users.noreply.github.com>
2021-05-10 12:35:46 -0400

    Update Maze.java

--- AP-CS_Final Project/src/Maze.java
+++ AP-CS_Final Project/src/Maze.java
@@ -1,10 +1,11 @@
 import java.awt.Point;//import library
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Maze {
 	//declare all the require variables
-	private static final int width = 100;
-	private static final int height= 500;
+	private static final int width = 60;
+	private static final int height= 40;
 	private int[][] board;
 	private Point door;
 	private boolean isSolved;
@@ -12,16 +13,41 @@ public class Maze {
 	private ArrayList<Enemy> enemies;
 	private ArrayList<Treasure> treasure;
 	private static int level;
-	//constructor
-	public Maze(int[][] board, Point door, boolean isSolved, User user, ArrayList<Enemy> enemies,
+	//constructors
+	public Maze(Point door, boolean isSolved, User user, ArrayList<Enemy> enemies,
 			ArrayList<Treasure> treasure) {
 		super();
-		this.board = board;
 		this.door = door;
 		this.isSolved = isSolved;
 		this.user = user;
 		this.enemies = enemies;
 		this.treasure = treasure;
+	}
+	
+	public Maze() {
+		super();
+		
+	}
+
+	//make a 2D array, random generate a maze
+	public boolean generateMaze() {
+		int[][] board = new int[height][width];
+		for (int i = 0;i<board[0].length;i+=board.length-1) {
+			for (int j=0; j<board.length;j++) {
+				 board[j][i]=1;
+			}
+		}
+		for (int i = 0;i<board.length;i+=board[0].length-1) {
+			for (int j=0; j<board[0].length;j++) {
+				 board[j][i]=1;
+			}
+		}
+		System.out.println(Arrays.deepToString(board));
+		
+	}
+	public boolean checkIfSolved() {
+		
+		
 	}
     //getters and setters
 	public int[][] getBoard() {
@@ -91,3 +117,6 @@ public class Maze {
 	
 	
 }
+
+
+
