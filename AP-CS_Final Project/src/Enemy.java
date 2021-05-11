fac4b7c9759f4ceb07c2f26aeb92baa62b83a31b
ramananj34 <77083795+ramananj34@users.noreply.github.com>
2021-05-11 19:16:47 -0400

    Update Enemy.java

--- AP-CS_Final Project/src/Enemy.java
+++ AP-CS_Final Project/src/Enemy.java
@@ -16,19 +16,19 @@ public class Enemy //Enemy class
 		this.speed = speed; //
 	} //All paramater constructor end
 
-	public ArrayList<Object> getGraphicsInfo()
-	{
-		return null;
-	}
+	public ArrayList<Object> getGraphicsInfo() //Getter for the graphics info
+	{ //Getter start
+		return null; //Returns null because I don't know how to do that
+	} //Getter end
 
-	public int getSpeed()
-	{
-		return speed;
-	}
+	public int getSpeed() //Speed getter
+	{ //Getter start
+		return speed; //Returns the speed
+	} //Getter end
 
-	public void setSpeed(int speed)
-	{
-		this.speed = speed;
-	}
+	public void setSpeed(int speed) //Speed setter
+	{ //Setter start
+		this.speed = speed; //Updates the speed
+	} //Seter end
 
 } //Enemy end
