3f8262d78a6fa9e5821cbdd29ddcdbc1779f27ad
wiztn <34725712+wiztn@users.noreply.github.com>
2021-06-04 03:27:53 -0400

    Delete AP-CS_Final Project directory

--- AP-CS_Final Project/src/Direction.java
+++
@@ -1,8 +0,0 @@
-//An enum for the direction to move in for code clarity. Containds left, right, up, and down
-
-public enum Direction //Direction Enum
-{ //Direction Start
-	
-	UP, DOWN, LEFT, RIGHT //Creates the static options for the enum
-	
-} //Direction End
